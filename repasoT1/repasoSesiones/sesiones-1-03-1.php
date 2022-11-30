2c7ac0fa666151033e6c160a06c82d64699a12ae
dirky7 <dhurper0905@g.educaand.es>
2022-11-30 18:15:39 +0100

    sesiones 3 a medio hacer

--- repasoT1/repasoSesiones/sesiones-1-03-1.php
+++ repasoT1/repasoSesiones/sesiones-1-03-1.php
@@ -1,43 +1,59 @@
 <?php
-/**
- * Sesiones (1) 03 - sesiones-1-03-1.php
- *
- * @author Escriba aquí su nombre
- *
- */
 
-
-
+	/**
+	 * Sesiones (1) 03 - sesiones-1-03-1.php
+	 *
+	 * @author Escriba aquí su nombre
+	 *
+	 */
+	session_name("sesion3");
+	session_start();
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
-  <meta charset="utf-8">
-  <title>
-    Formulario Palabra en mayúsculas (Formulario).
-    Sesiones (1). Sesiones.
-    Escriba aquí su nombre
-  </title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="ejercicios.css" title="Color">
+	<meta charset="utf-8">
+	<title>
+		Formulario Palabra en mayúsculas (Formulario).
+		Sesiones (1). Sesiones.
+		Escriba aquí su nombre
+	</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="ejercicios.css" title="Color">
 </head>
 
 <body>
-  <h1>Formulario Palabra en mayúsculas (Formulario)</h1>
+	<h1>Formulario Palabra en mayúsculas (Formulario)</h1>
 
-  <form action="sesiones-1-03-2.php" method="get">
+	<form action="sesiones-1-03-2.php" method="get">
 
-<?php
+		<?php
+			if (isset($_SESSION['texto']))
+			{
+				if (!ctype_lower($_SESSION['texto']))
+				{
+					print("<p>El texto es: <strong>$_SESSION[texto]</strong></p>");
+				}
+				else
+				{
+					print("<p><strong>ERROR - Minuscula introducida</strong></p>");
+				}
+			}
+		?>
+		<p>
+			<label for="mayus">Escribe una palabra en MAYUS</label>
+			<input type="text" name="texto" id="mayus">
+		</p>
+		<p>
+			<input type="submit" value="Comprobar">
+			<input type="reset" value="Borrar">
+		</p>
+	</form>
 
-?>
-    <p>
-      <input type="submit" value="Comprobar">
-      <input type="reset" value="Borrar">
-    </p>
-  </form>
-
-  <footer>
-    <p>Escriba aquí su nombre</p>
-  </footer>
+	<footer>
+		<p>Escriba aquí su nombre</p>
+	</footer>
 </body>
+
 </html>
--- repasoT1/repasoSesiones/sesiones-1-03-2.php
+++ repasoT1/repasoSesiones/sesiones-1-03-2.php
@@ -1,44 +1,48 @@
 <?php
-/**
- * Sesiones (1) 03 - sesiones-1-03-2.php
- *
- * @author Escriba aquí su nombre
- *
- */
+	/**
+	 * Sesiones (1) 03 - sesiones-1-03-2.php
+	 *
+	 * @author Escriba aquí su nombre
+	 *
+	*/
+	session_name("sesion3");
+	session_start();
+	// Funciones auxiliares 1
+	function recoge($var, $m = "")
+	{
+		$tmp = is_array($m) ? [] : "";
+		if (isset($_REQUEST[$var])) {
+			if (!is_array($_REQUEST[$var]) && !is_array($m)) {
+				$tmp = trim(htmlspecialchars($_REQUEST[$var]));
+			} elseif (is_array($_REQUEST[$var]) && is_array($m)) {
+				$tmp = $_REQUEST[$var];
+				array_walk_recursive($tmp, function (&$valor) {
+					$valor = trim(htmlspecialchars($valor));
+				});
+			}
+		}
+		return $tmp;
+	}
+
+	// Recogemos la palabra
+	$texto = $_GET['texto'];
+	if (!empty($texto))
+	{
+		$_SESSION['texto'] = $texto;
+	}
+
+	// Guardamos la palabra en la sesión 
 
 
+	// Comprobamos la palabra
 
-// Funciones auxiliares 1
-function recoge($var, $m = "")
-{
-    $tmp = is_array($m) ? [] : "";
-    if (isset($_REQUEST[$var])) {
-        if (!is_array($_REQUEST[$var]) && !is_array($m)) {
-            $tmp = trim(htmlspecialchars($_REQUEST[$var]));
-        } elseif (is_array($_REQUEST[$var]) && is_array($m)) {
-            $tmp = $_REQUEST[$var];
-            array_walk_recursive($tmp, function (&$valor) {
-                $valor = trim(htmlspecialchars($valor));
-            });
-        }
-    }
-    return $tmp;
-}
+		// Si no se recibe palabra, guardamos en la sesión el mensaje de error 
 
-// Recogemos la palabra
+		// Si la palabra está en minúsculas, guardamos en la sesión el mensaje de error 
+
+		// Si la palabra es correcta, borramos el error anterior que pudiera haber en la sesión 
 
 
-// Guardamos la palabra en la sesión 
-
-
-// Comprobamos la palabra
-
-    // Si no se recibe palabra, guardamos en la sesión el mensaje de error 
-
-    // Si la palabra está en minúsculas, guardamos en la sesión el mensaje de error 
-
-    // Si la palabra es correcta, borramos el error anterior que pudiera haber en la sesión 
-
-
-// Volvemos al formulario 
-header("Location:sesiones-1-03-1.php");
+	// Volvemos al formulario 
+	header("Location:sesiones-1-03-1.php");
+?>
