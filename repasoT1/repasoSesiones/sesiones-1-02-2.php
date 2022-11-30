9ae79c12fdcc9369d8e37c054826bda55f0fee13
dirky7 <dhurper0905@g.educaand.es>
2022-11-30 17:59:04 +0100

    sesiones 1 y 2 hecho

--- repasoT1/repasoSesiones/sesiones-1-02-2.php
+++ repasoT1/repasoSesiones/sesiones-1-02-2.php
@@ -5,38 +5,43 @@
  * @author Escriba aquí su nombre
  *
  */
+	session_name("sesion2");
+	session_start();
+	// Funciones auxiliares 1
+	function recoge($var, $m = "")
+	{
+		$tmp = is_array($m) ? [] : "";
+		if (isset($_REQUEST[$var])) {
+			if (!is_array($_REQUEST[$var]) && !is_array($m)) {
+				$tmp = trim(htmlspecialchars($_REQUEST[$var]));
+			} elseif (is_array($_REQUEST[$var]) && is_array($m)) {
+				$tmp = $_REQUEST[$var];
+				array_walk_recursive($tmp, function (&$valor) {
+					$valor = trim(htmlspecialchars($valor));
+				});
+			}
+		}
+		return $tmp;
+	}
+
+	// Recogemos el texto
+	$texto = $_GET['texto'];
+	if (!empty($texto))
+	{
+		$_SESSION['texto'] = $texto;
+	}
+	header("Location:sesiones-1-02-1.php")
+
+	// Comprobamos el texto
+
+		// Si el texto es vacío, no es correcto, pero no hacemos nada especial 2
 
 
+	// Si el texto no es válido ...
 
-// Funciones auxiliares 1
-function recoge($var, $m = "")
-{
-    $tmp = is_array($m) ? [] : "";
-    if (isset($_REQUEST[$var])) {
-        if (!is_array($_REQUEST[$var]) && !is_array($m)) {
-            $tmp = trim(htmlspecialchars($_REQUEST[$var]));
-        } elseif (is_array($_REQUEST[$var]) && is_array($m)) {
-            $tmp = $_REQUEST[$var];
-            array_walk_recursive($tmp, function (&$valor) {
-                $valor = trim(htmlspecialchars($valor));
-            });
-        }
-    }
-    return $tmp;
-}
+		// ... volvemos al formulario 
 
-// Recogemos el texto
+		// ... guardamos el texto en la sesión 
 
-
-// Comprobamos el texto
-
-    // Si el texto es vacío, no es correcto, pero no hacemos nada especial 2
-
-
-// Si el texto no es válido ...
-
-    // ... volvemos al formulario 
-
-    // ... guardamos el texto en la sesión 
-
-    // Volvemos al formulario 
+		// Volvemos al formulario 
+?>
