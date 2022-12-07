24f68b492a39ed2f2b5505985b3a044d162115cb
dirky7 <dhurper0905@g.educaand.es>
2022-12-07 17:55:28 +0100

    progresos

--- Tarea4/admin/nuevo_usuario.php
+++ Tarea4/admin/nuevo_usuario.php
@@ -1,5 +1,32 @@
 <?php
-
+	include "../inc/header.inc.php";
+	$mensaje_error = "";
+	if (isset($_POST['form_new_user']))
+	{
+		$login = $_POST['login'];
+		$password = $_POST['password'];
+		$repassword = $_POST['repassword'];
+		$nombre = $_POST['nombre'];
+		$fNacimiento = $_POST['fNacimiento'];
+		$presupuesto = $_POST['presupuesto'];
+		$nuevo_usuario = array(
+			'login' => $login,
+			'password' => $password,
+			'repassword' => $repassword,
+			'nombre' => $nombre,
+			'fNacimiento' => $fNacimiento,
+			'presupuesto' => $presupuesto
+		);
+		$errores = errorValidarDatosUsuario($nuevo_usuario);
+		if ($errores == false)
+		{
+			$mensaje_error = "<div>Perfecto!</div>";
+		}
+		else
+		{
+			$mensaje_error = "<div class='error'>" . $errores . "</div>";
+		}
+	}
 ?>
 <!DOCTYPE html>
 <html>
@@ -12,11 +39,11 @@
 	<header><h1>Creación de Usuarios</h1></header>
 	<nav>
 		<span class="desplegable">
-			<a href="./?<?php echo $fakeCookie; ?>">Administrar Usuarios</a>
+			<a href="index.php">Administrar Usuarios</a>
 			<div>
-				<a href="nuevo_usuario.php?<?php  ?>">Nuevo Usuario</a>
-				<a href="modificar_usuario.php?<?php  ?>">Modificar Usuario</a>
-				<a href="borrar_usuario.php?<?php  ?>">Borrar Usuario</a>
+				<a href="nuevo_usuario.php">Nuevo Usuario</a>
+				<a href="modificar_usuario.php">Modificar Usuario</a>
+				<a href="borrar_usuario.php">Borrar Usuario</a>
 				<a href="../">Salir</a>
 			</div>
 		</span>
@@ -28,30 +55,33 @@
 			if (!empty($error)) {echo "<div class='error'><b>!</b>$error</div>";}
 			if (!empty($correcto)) {echo "<div class='correcto'><b>!</b>$correcto</div>";}
 			?>
-			<form method="post" action="?<?php echo $fakeCookie; ?>">
+			<form method="post" action="">
+				<?php
+					echo $mensaje_error;
+				?>
 				<div class="input-labeled">
 					<label>Login:</label>
-					<input type="text" name="newUser[login]" required maxlength="10" value="<?php  ?>">
+					<input type="text" name="login" required maxlength="10">
 				</div>
 				<div class="input-labeled">
 					<label>Clave:</label>
-					<input type="password" name="newUser[password]" required maxlength="20" value="<?php  ?>">
+					<input type="password" name="password" required maxlength="20">
 				</div>
 				<div class="input-labeled">
 					<label>Repite Clave:</label>
-					<input type="password" name="newUser[repassword]" required maxlength="20" value="<?php  ?>">
+					<input type="password" name="repassword" required maxlength="20">
 				</div>
 				<div class="input-labeled">
 					<label>Nombre:</label>
-					<input type="text" name="newUser[nombre]" required maxlength="30" value="<?php  ?>">
+					<input type="text" name="nombre" required maxlength="30">
 				</div>
 				<div class="input-labeled">
 					<label>Fecha Nacimiento:</label>
-					<input type="date" name="newUser[fNacimiento]" placeholder="aaaa-mm-dd" required maxlength="10" value="<?php  ?>">
+					<input type="date" name="fNacimiento" placeholder="aaaa-mm-dd" required maxlength="10">
 				</div>
 				<div class="input-labeled">
 					<label>Presupuesto:</label>
-					<input type="text" name="newUser[presupuesto]" required maxlength="30" value="<?php  ?>">
+					<input type="text" name="presupuesto" required maxlength="30">
 				</div>
 				<input type="submit" name="form_new_user" value="Crear Usuario">
 			</form>
